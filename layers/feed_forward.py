1434895a845767e2197504eb199255404ea86855
temilaj <temi@temilajumoke.com>
2024-08-18 21:02:08 -0700

    add feed forward:  multi-layer perceptron module

---
+++ layers/feed_forward.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+from config import get_config
+
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        config = get_config()
+        dropout = config['dropout']
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
